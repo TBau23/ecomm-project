5b448010255968a68b3b2fdd4d7c6e1eccef8b6f
Tom Bauer <tbauer1898@gmail.com>
2021-01-26 11:34:19 -0800

    Header built

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shoppage/ShopPage';
 import { Switch, Route } from 'react-router-dom'
+import Header from './components/header/Header'
 
 
 
 function App() {
-  // switch statement causes routing to match with first route that has any sort of match
+  // switch statement prevents rendering of multiple components that match path
+  // can avoid switch effect with, say, a header, by putting it outside of switch
   return (
     <div className="App">
       
-      <Switch>
+      <Header />
+      <Switch> 
         <Route exact path='/' component={Homepage}/>
         <Route path='/shop' component={ShopPage} />
       </Switch>
